aa8a9553ce46b364db1944b7bd46ede07d54674e
bottomhalf <bottomhalf.dev@gmail.com>
2022-05-27 08:39:25 +0530

    added new fields

--- ModalLayer/Modal/Accounts/SalaryComponents.cs
+++ ModalLayer/Modal/Accounts/SalaryComponents.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ModalLayer.Modal.Accounts
+﻿namespace ModalLayer.Modal.Accounts
 {
     public class SalaryComponents : CreationInfo
     {
@@ -11,10 +7,11 @@ namespace ModalLayer.Modal.Accounts
         public bool CalculateInPercentage { set; get; }
         public decimal PercentageValue { set; get; }
         public decimal Amount { set; get; }
-        public bool EmployeeContribution { set; get; }
-        public bool EmployerContribution { set; get; }
+        public decimal EmployeeContribution { set; get; }
+        public decimal EmployerContribution { set; get; }
         public bool IncludeInPayslip { set; get; }
         public bool IsDeductions { set; get; }
         public bool IsOpted { set; get; }
+        public bool IsActive { set; get; }
     }
 }
