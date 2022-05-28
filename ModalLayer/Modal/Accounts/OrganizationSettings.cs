e67e35de22c189cea3421c2a9791ba65f355ef6b
Marghub <marghub12@gmail.com>
2022-05-28 10:08:24 +0530

    payroll and  organization model created

--- ModalLayer/Modal/Accounts/OrganizationSettings.cs
+++ ModalLayer/Modal/Accounts/OrganizationSettings.cs
@@ -6,5 +6,11 @@ namespace ModalLayer.Modal.Accounts
 {
     public class OrganizationSettings
     {
+        public string LegalEntity {get; set;}
+        public string Signature {get; set;}
+        public string CompanyLegalName {get; set;}
+        public string BusinessType {get; set;}
+        public string InformationDate {get; set;}
+        public string RegisteredAddress { get; set; }
     }
 }
