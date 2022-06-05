fa5410808425d4b016d5ea6ea6a60b7523955d3a
bottomhalf <bottomhalf.dev@gmail.com>
2022-06-05 10:10:31 +0530

    common class for salary

--- ModalLayer/Modal/Accounts/SalaryStructure.cs
+++ ModalLayer/Modal/Accounts/SalaryStructure.cs
@@ -4,12 +4,11 @@ using System.Text;
 
 namespace ModalLayer.Modal.Accounts
 {
-    public class SalaryStructure:CreationInfo
+    public class SalaryStructure : SalaryCommon
     {
         public string ComponentName { get; set; }
         public string Type { get; set; }
         public string TaxExempt { get; set; }
-        public string MaxLimit { get; set; }
         public bool RequireDocs { get; set; }
         public bool IndividualOverride { get; set; }
         public bool IsComponentEnable { get; set; }
