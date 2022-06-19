367b2f0a23d1164221f4b8d8ad0f3963e1fa06b5
bottomhalf <bottomhalf.dev@gmail.com>
2022-06-19 07:50:58 +0530

    Declaration files

--- ModalLayer/Modal/Files.cs
+++ ModalLayer/Modal/Files.cs
@@ -46,6 +46,7 @@ namespace ModalLayer.Modal
         public string FilePath { set; get; }
         public long FileOwnerId { set; get; }
         public string FileName { set; get; }
+        public string AlternateName { set; get; }
         public string FileExtension { set; get; }
         public string Status { set; get; }
         public DateTime? PaidOn { set; get; }
@@ -55,6 +56,7 @@ namespace ModalLayer.Modal
         public string Email { set; get; }
         public string FileType { set; get; }
         public string FileSize { set; get; }
+        public long FileId { set; get; }
         public string ParentFolder { set; get; } = null;
         public FileSystemType SystemFileType { set; get; }
     }
