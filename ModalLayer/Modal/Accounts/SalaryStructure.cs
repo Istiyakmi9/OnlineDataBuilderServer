d9262a787f7e4d11994da00ff43b03b5ce0038eb
bottomhalf <bottomhalf.dev@gmail.com>
2022-06-21 07:43:32 +0530

    bug fixes

--- ModalLayer/Modal/Accounts/SalaryStructure.cs
+++ ModalLayer/Modal/Accounts/SalaryStructure.cs
@@ -7,7 +7,7 @@ namespace ModalLayer.Modal.Accounts
     public class SalaryStructure : SalaryCommon
     {
         public string ComponentName { get; set; }
-        public string Type { get; set; }
+        public int ComponentTypeId { get; set; }
         public bool IndividualOverride { get; set; }
         public bool IsComponentEnable { get; set; }
         public bool IsAllowtoOverride { get; set; }
