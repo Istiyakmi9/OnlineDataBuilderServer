afd2dc1b1fe4e89efe56da44dbfa194036d0f91e
Marghub <marghub12@gmail.com>
2022-07-23 13:03:49 +0530

    Leave Quota end point added

---
+++ ModalLayer/Modal/Leaves/LeaveQuota.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModalLayer.Modal.Leaves
+{
+    public class LeaveQuota
+    {
+        public bool IsQuotaLimit { get; set; }
+        public int QuotaLimit { get; set; }
+        public bool IsBeyondAnnualQuota { get; set; }
+        public int BeyondAnnualQuota { get; set; }
+        public bool IsLeaveQuotaAllocated { get; set; }
+        public int LeaveQuotaAllocatedAfter { get; set; }
+        public bool IsManagerAwardCasual { get; set; }
+    }
+}
