5dfd99d734a31eb252e1652abb28ac9e62c54c98
Marghub <marghub12@gmail.com>
2022-08-19 16:02:57 +0530

    CompanySetting Modal added

---
+++ ModalLayer/Modal/CompanySetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModalLayer.Modal
+{
+    public class CompanySetting
+    {
+        public int SettingId {set; get;}
+        public int CompanyId {set; get;}
+        public int ProbationPeriodInDays {set; get;}
+        public int NoticePeriodInDays { set; get; }
+    }
+}
