753537fda46c8120e25dfb266dbacaea0aacaf97
istiyakmi9 <istiyaq.mi9@gmail.com>
2022-09-17 23:21:47 +0530

    email context change

--- ModalLayer/Scripts/alter_script.sql
+++ ModalLayer/Scripts/alter_script.sql
@@ -13,3 +13,25 @@ drop column LeaveQuotaStatus;
 
 alter table employee_leave_request
 add column LeaveQuotaDetail json;
+
+drop table if exists email_setting_detail;
+
+create table email_setting_detail(
+	EmailSettingDetailId int primary key auto_increment,
+	CompanyId int,
+    EmailAddress varchar(200),
+    EmailName varchar(100),
+    EmailHost varchar(100),
+    PortNo int,
+    EnableSsl bit,
+    DeliveryMothod varchar(50),
+    UserDefaultCredentials bit,
+    Credentials varchar(100),
+    IsPrimary bit,
+    UpdatedBy long,
+    UpdatedOn Datetime not null
+);
+
+select * from company;
+select * from email_setting_detail;
+insert into email_setting_detail values(default, 1, 'info@bottomhalf.in', 'BOTTOMHALF', 'smtpout.asia.secureserver.net', 587, true, 0, false, 'bottomhalf@mi9', true, 22, now());
