8e9560c1a3f5be86bbd24650a1ef138c8b2d043a
Marghub <marghub12@gmail.com>
2023-03-01 11:17:25 +0530

    bug fixed

--- ServiceLayer/Code/LeaveCalculation.cs
+++ ServiceLayer/Code/LeaveCalculation.cs
@@ -303,7 +303,6 @@ namespace ServiceLayer.Code
                                  select new
                                  {
                                      EmployeeId = r.EmployeeUid,
-                                     //LeaveRequestId = r.LeaveRequestId,
                                      Year = _timezoneConverter.ToTimeZoneDateTime(DateTime.UtcNow, _currentSession.TimeZone).Year,
                                      LeaveTypeBriefJson = JsonConvert.SerializeObject(r.LeaveTypeBrief)
                                  }).ToList();
