ddaa24af1555bc318144098f0271dad92d6788ce
Marghub <marghub12@gmail.com>
2023-03-13 20:40:39 +0530

    bug fixed

--- ServiceLayer/Interface/ITimesheetService.cs
+++ ServiceLayer/Interface/ITimesheetService.cs
@@ -12,7 +12,7 @@ namespace ServiceLayer.Interface
         Task<TimesheetDetail> SaveTimesheetService(TimesheetDetail timesheetDetail);
         Task<string> ExecuteActionOnTimesheetService(TimesheetDetail timesheetDetail);
         List<TimesheetDetail> GetPendingTimesheetByIdService(long employeeId, long clientId);
-        dynamic GetEmployeeTimeSheetService(TimesheetDetail timesheetDetail);
+        List<DailyTimesheetDetail> GetEmployeeTimeSheetService(TimesheetDetail timesheetDetail);
         (List<DailyTimesheetDetail>, List<DateTime>) BuildFinalTimesheet(TimesheetDetail currentTimesheetDetail);
         BillingDetail EditEmployeeBillDetailService(GenerateBillFileDetail fileDetail);
         Task RunWeeklyTimesheetCreation(DateTime TimesheetStartDate);
